931e7a9e81373fec70936aead842672541ba6bb3
Desmond Ho <desmondh268@gmail.com>
2022-10-21 21:42:16 -0700

    added live chart functionaliy, very beta

--- TRIUMF-frontend/chart.js
+++ TRIUMF-frontend/chart.js
@@ -1,79 +1,100 @@
-const ctx = document.getElementById('myChart');
+const ctx = document.getElementById("myChart");
+
+let data_array = [];
+
+function UpdateGraph() {
+  const data_value = document.getElementById("graph_data").innerText;
+  console.log(data_value);
+  data_array.push(data_value);
+  if (data_array.length > 13) {
+    data_array.shift();
+  }
+}
+
+setInterval(UpdateGraph, 5000);
+
 const myChart = new Chart(ctx, {
-  type: 'line',
+  type: "line",
   data: {
     labels: [
-      '0',
-      '1',
-      '2',
-      '3',
-      '4',
-      '5',
-      '6',
-      '7',
-      '8',
-      '9',
-      '10',
-      '11',
-      '12'
+      "0",
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "11",
+      "12",
     ],
+
     datasets: [
       {
-        label: 'Voltage over 5 Second Intervals',
-        data: [12, 19, 3, 5, 2, 3, 12, 19, 3, 5, 2, 3, 0],
+        label: "Voltage over 5 Second Intervals",
+        data: data_array,
         backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)'
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
         ],
         borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)'
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)",
         ],
-        borderWidth: 1
-      }
-    ]
+        borderWidth: 1,
+      },
+    ],
   },
   options: {
     responsive: true,
     plugins: {
       legend: {
-        display: false
+        display: false,
       },
       title: {
         display: true,
-        text: 'Voltage per 5 Second Cycle',
+        text: "Dr.Chales Fav Variable, IOS:FC6:SCALECUR",
         font: {
-          size: 25
-        }
-      }
+          size: 25,
+        },
+      },
     },
     scales: {
       x: {
         grid: {
-          display: false
+          display: false,
         },
         ticks: {
           font: {
-            size: 25
-          }
-        }
+            size: 25,
+          },
+        },
       },
       y: {
         beginAtZero: true,
         ticks: {
           font: {
-            size: 25
-          }
-        }
-      }
-    }
-  }
+            size: 25,
+          },
+        },
+      },
+    },
+  },
 });
+
+function updateChart(chart) {
+  chart.update();
+}
+
+setInterval(updateChart, 5000, myChart);
--- TRIUMF-frontend/data.js
+++ TRIUMF-frontend/data.js
@@ -1,38 +1,38 @@
 function fetchData() {
   const data = {
     readPvDict: {
-      'IOS:BEAMTYPE': '1',
-      'IOS:BIAS:VOL': '12636.987869077593',
-      'IOS:HALLMB:RDFIELD': '34.916000000000054',
-      'IOS:IG1:RDVAC': '7.941049439712669e-08',
-      'IOS:IG2:RDVAC': '1.9525325860315103e-07',
-      'IOS:MB:MASSOVERQ2': '3.9764078010664123',
-      'ISAC:IOSBEAMPATH': 'ios-hebt2-dragon',
-      'MCIS:BIAS0:VOL': '12637.216754406041',
-      'MCIS:EL1:VOL': '0.0',
-      'MCIS:IG1:RDVAC': '3.2331256866455076e-07',
-      'MCIS:RFS1:AMPL': '18.48680000000001',
-      'MCIS:RFS1:FREQ': '13.04530000000061',
-      'MCIS:RFS2:AMPL': '17.864000000000004',
-      'MCIS:RFS2:FREQ': '13.828357500000033'
+      "IOS:BEAMTYPE": "1",
+      "IOS:BIAS:VOL": "12636.987869077593",
+      "IOS:HALLMB:RDFIELD": "34.916000000000054",
+      "IOS:IG1:RDVAC": "7.941049439712669e-08",
+      "IOS:IG2:RDVAC": "1.9525325860315103e-07",
+      "IOS:MB:MASSOVERQ2": "3.9764078010664123",
+      "ISAC:IOSBEAMPATH": "ios-hebt2-dragon",
+      "MCIS:BIAS0:VOL": "12637.216754406041",
+      "MCIS:EL1:VOL": "0.0",
+      "MCIS:IG1:RDVAC": "3.2331256866455076e-07",
+      "MCIS:RFS1:AMPL": "18.48680000000001",
+      "MCIS:RFS1:FREQ": "13.04530000000061",
+      "MCIS:RFS2:AMPL": "17.864000000000004",
+      "MCIS:RFS2:FREQ": "13.828357500000033",
     },
     readUnitsDict: {
-      'IOS:BEAMTYPE': null,
-      'IOS:BIAS:VOL': 'V',
-      'IOS:HALLMB:RDFIELD': 'G',
-      'IOS:IG1:RDVAC': 'T',
-      'IOS:IG2:RDVAC': 'T',
-      'IOS:MB:MASSOVERQ2': 'units',
-      'ISAC:IOSBEAMPATH': null,
-      'MCIS:BIAS0:VOL': 'V',
-      'MCIS:EL1:VOL': 'V',
-      'MCIS:IG1:RDVAC': 'T',
-      'MCIS:RFS1:AMPL': 'dB',
-      'MCIS:RFS1:FREQ': 'GHz',
-      'MCIS:RFS2:AMPL': 'dB',
-      'MCIS:RFS2:FREQ': 'GHz'
+      "IOS:BEAMTYPE": null,
+      "IOS:BIAS:VOL": "V",
+      "IOS:HALLMB:RDFIELD": "G",
+      "IOS:IG1:RDVAC": "T",
+      "IOS:IG2:RDVAC": "T",
+      "IOS:MB:MASSOVERQ2": "units",
+      "ISAC:IOSBEAMPATH": null,
+      "MCIS:BIAS0:VOL": "V",
+      "MCIS:EL1:VOL": "V",
+      "MCIS:IG1:RDVAC": "T",
+      "MCIS:RFS1:AMPL": "dB",
+      "MCIS:RFS1:FREQ": "GHz",
+      "MCIS:RFS2:AMPL": "dB",
+      "MCIS:RFS2:FREQ": "GHz",
     },
-    timestamp: '2022-10-07 11:36:01'
+    timestamp: "2022-10-07 11:36:01",
   };
   console.log(data);
 
@@ -40,7 +40,7 @@ function fetchData() {
 }
 
 function removeFiller() {
-  const fillerTableItems = document.querySelectorAll('.original');
+  const fillerTableItems = document.querySelectorAll(".original");
   fillerTableItems.forEach((element) => {
     element.remove();
   });
@@ -50,18 +50,18 @@ async function mapData(data) {
   removeFiller();
   const listPVDict = Object.entries(await getLocalOlisData());
   const listUnitsDict = Object.entries(data.readUnitsDict);
-  const tableBody = document.querySelector('tbody');
+  const tableBody = document.querySelector("tbody");
   for (const [index, value] of listPVDict.entries()) {
-    const row = document.createElement('tr');
-    row.className = 'original';
+    const row = document.createElement("tr");
+    row.className = "original";
     tableBody.appendChild(row);
     for (const property of value) {
-      const tableCell = document.createElement('td');
+      const tableCell = document.createElement("td");
       tableCell.textContent += property;
       row.appendChild(tableCell);
     }
 
-    const tableCell = document.createElement('td');
+    const tableCell = document.createElement("td");
     tableCell.textContent += listUnitsDict[index][1];
     row.appendChild(tableCell);
   }
@@ -69,32 +69,44 @@ async function mapData(data) {
 
 async function updateSecondColumnTableValues() {
   const data = await getLocalOlisData();
+  const graph_data = await getGraphData();
   const listPVDict = Object.entries(data);
-  const tableBody = document.querySelector('tbody');
-  const tableRows = tableBody.querySelectorAll('tr');
+  const tableBody = document.querySelector("tbody");
+  const tableRows = tableBody.querySelectorAll("tr");
   tableRows.forEach((row, index) => {
-    const tableCell = row.querySelector('td:nth-child(2)');
+    const tableCell = row.querySelector("td:nth-child(2)");
     tableCell.textContent = listPVDict[index][1];
   });
+
+  const invis_data = document.querySelector("#graph_data");
+  invis_data.innerHTML = graph_data["IOS:FC6:SCALECUR"];
 }
 
 function insertTime() {
   const d = new Date();
-  const element = document.querySelector('.time');
+  const element = document.querySelector(".time");
   element.textContent = `${d.toTimeString()}`;
 }
 
 async function getLocalData() {
-  const response = await fetch('http://127.0.0.1:8081/', {
-    method: 'GET'
+  const response = await fetch("http://127.0.0.1:8081/", {
+    method: "GET",
   });
   const freshData = await response.json();
   return freshData;
 }
 
 async function getLocalOlisData() {
-  const response = await fetch('http://127.0.0.1:8081/OLIS', {
-    method: 'GET'
+  const response = await fetch("http://127.0.0.1:8081/OLIS", {
+    method: "GET",
+  });
+  const freshData = await response.json();
+  return freshData;
+}
+
+async function getGraphData() {
+  const response = await fetch("http://127.0.0.1:8081/ChartData", {
+    method: "GET",
   });
   const freshData = await response.json();
   return freshData;
