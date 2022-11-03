b168122b219d955fffa681493121c5957aeee293
kitoshi <kitoshi.charlton@gmail.com>
2022-11-03 14:18:03 -0700

    twitter font size + chart goes 15 minutes

--- TRIUMF-frontend/chart.js
+++ TRIUMF-frontend/chart.js
@@ -1,111 +1,119 @@
-const ctx = document.getElementById("myChart");
+const ctx = document.getElementById('myChart');
 
 let dataArray = [];
 
-let labelArray = Array.from({ length: 50 }, (x, i) => i + 5);
+let labelArray = Array.from({ length: 180 }, (x, i) => i + 5);
 
 let cycles = 0;
 
+let index = 0;
+
 function updateGraph() {
-  const dataValue = document.getElementById("graph_data").innerText;
+  const dataValue = document.getElementById('graph_data').innerText;
   dataArray.push(dataValue.slice(0, 2));
-  if (dataArray.length > 50) {
+  if (dataArray.length > 180) {
     dataArray.shift();
   }
   cycles++;
-  if (cycles > 50) {
+  if (cycles > 180) {
     updateLabel();
   }
 }
 
 function updateLabel() {
-  labelArray.push(Number(labelArray.at(-1)).toString());
-  labelArray.shift();
+  if (cycles > 180) {
+    labelArray.push(Number(labelArray[index]).toString());
+    if (index > 180) {
+      index = 0;
+    }
+    index++;
+    labelArray.shift();
+  }
 }
 
 setInterval(updateGraph, 5000);
 
 const myChart = new Chart(ctx, {
-  type: "line",
+  type: 'line',
   data: {
     labels: labelArray,
     datasets: [
       {
-        label: "Voltage over 5 Second Intervals",
+        label: 'Voltage over 5 Second Intervals',
         data: dataArray,
         backgroundColor: [
-          "rgba(255, 99, 132, 0.2)",
-          "rgba(54, 162, 235, 0.2)",
-          "rgba(255, 206, 86, 0.2)",
-          "rgba(75, 192, 192, 0.2)",
-          "rgba(153, 102, 255, 0.2)",
-          "rgba(255, 159, 64, 0.2)",
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
         ],
         borderColor: [
-          "rgba(255, 99, 132, 1)",
-          "rgba(54, 162, 235, 1)",
-          "rgba(255, 206, 86, 1)",
-          "rgba(75, 192, 192, 1)",
-          "rgba(153, 102, 255, 1)",
-          "rgba(255, 159, 64, 1)",
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
         ],
-        borderWidth: 1,
-      },
-    ],
+        borderWidth: 1
+      }
+    ]
   },
   options: {
     responsive: true,
     plugins: {
       legend: {
-        display: false,
+        display: false
       },
       title: {
         display: true,
-        text: "Faraday Cup 6 (FC6) Current, per 5 Second Intervals",
+        text: 'Faraday Cup 6 (FC6) Current, per 5 Second Intervals',
         font: {
-          size: 25,
-        },
-      },
+          size: 25
+        }
+      }
     },
     scales: {
       x: {
         grid: {
-          display: false,
+          display: false
         },
         ticks: {
           font: {
-            size: 25,
+            size: 25
           },
           callback: function (val, index) {
             // Hide every 2nd tick label
-            return index % 2 === 0 ? this.getLabelForValue(val) : "";
-          },
+            return index % 2 === 0 ? this.getLabelForValue(val) : '';
+          }
         },
         title: {
           display: true,
-          text: "Interval (5s)",
+          text: 'Interval (5s)',
           font: {
-            size: 25,
-          },
-        },
+            size: 25
+          }
+        }
       },
       y: {
         beginAtZero: true,
         ticks: {
           font: {
-            size: 25,
-          },
+            size: 25
+          }
         },
         title: {
           display: true,
-          text: "Amps",
+          text: 'Amps',
           font: {
-            size: 25,
-          },
-        },
-      },
-    },
-  },
+            size: 25
+          }
+        }
+      }
+    }
+  }
 });
 
 function updateChart(chart) {
