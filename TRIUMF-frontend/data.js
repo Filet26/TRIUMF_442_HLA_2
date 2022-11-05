8f23d7ae639d1ba66d4cb5ee790c0ba7b2938164
kitoshi <kitoshi.charlton@gmail.com>
2022-11-04 18:34:45 -0700

    slide show

--- TRIUMF-frontend/data.js
+++ TRIUMF-frontend/data.js
@@ -2,38 +2,38 @@ function fetchData() {
   // dummy data
   const data = {
     readPvDict: {
-      "IOS:BEAMTYPE": "1",
-      "IOS:BIAS:VOL": "12636.987869077593",
-      "IOS:HALLMB:RDFIELD": "34.916000000000054",
-      "IOS:IG1:RDVAC": "7.941049439712669e-08",
-      "IOS:IG2:RDVAC": "1.9525325860315103e-07",
-      "IOS:MB:MASSOVERQ2": "3.9764078010664123",
-      "ISAC:IOSBEAMPATH": "ios-hebt2-dragon",
-      "MCIS:BIAS0:VOL": "12637.216754406041",
-      "MCIS:EL1:VOL": "0.0",
-      "MCIS:IG1:RDVAC": "3.2331256866455076e-07",
-      "MCIS:RFS1:AMPL": "18.48680000000001",
-      "MCIS:RFS1:FREQ": "13.04530000000061",
-      "MCIS:RFS2:AMPL": "17.864000000000004",
-      "MCIS:RFS2:FREQ": "13.828357500000033",
+      'IOS:BEAMTYPE': '1',
+      'IOS:BIAS:VOL': '12636.987869077593',
+      'IOS:HALLMB:RDFIELD': '34.916000000000054',
+      'IOS:IG1:RDVAC': '7.941049439712669e-08',
+      'IOS:IG2:RDVAC': '1.9525325860315103e-07',
+      'IOS:MB:MASSOVERQ2': '3.9764078010664123',
+      'ISAC:IOSBEAMPATH': 'ios-hebt2-dragon',
+      'MCIS:BIAS0:VOL': '12637.216754406041',
+      'MCIS:EL1:VOL': '0.0',
+      'MCIS:IG1:RDVAC': '3.2331256866455076e-07',
+      'MCIS:RFS1:AMPL': '18.48680000000001',
+      'MCIS:RFS1:FREQ': '13.04530000000061',
+      'MCIS:RFS2:AMPL': '17.864000000000004',
+      'MCIS:RFS2:FREQ': '13.828357500000033'
     },
     readUnitsDict: {
-      "IOS:BIAS:VOL": "V",
-      "IOS:BIAS:RDCUR": "mA",
-      "IOS:IZR:RDVOL": "V",
-      "MCIS:BIAS0:RDVOL": "V",
-      "MCIS:BIAS0:RDCUR": "uA",
-      "MCIS:RFS2:FREQ": "GHz",
-      "IOS:MB:MASSOVERQ2": "",
-      "IOS:MB:RDCUR": "A",
-      "IOS:HALLMB:RDFIELD": "G",
-      "IOS:IG1:RDVAC": "T",
-      "IOS:IG2:RDVAC": "T",
-      "MCIS:IG1:RDVAC": "T",
-      "MCIS:RFS2:AMPL": "dB",
-      "MCIS:RFS2:FREQ": "GHz",
+      'IOS:BIAS:VOL': 'V',
+      'IOS:BIAS:RDCUR': 'mA',
+      'IOS:IZR:RDVOL': 'V',
+      'MCIS:BIAS0:RDVOL': 'V',
+      'MCIS:BIAS0:RDCUR': 'uA',
+      'MCIS:RFS2:FREQ': 'GHz',
+      'IOS:MB:MASSOVERQ2': '',
+      'IOS:MB:RDCUR': 'A',
+      'IOS:HALLMB:RDFIELD': 'G',
+      'IOS:IG1:RDVAC': 'T',
+      'IOS:IG2:RDVAC': 'T',
+      'MCIS:IG1:RDVAC': 'T',
+      'MCIS:RFS2:AMPL': 'dB',
+      'MCIS:RFS2:FREQ': 'GHz'
     },
-    timestamp: "2022-10-07 11:36:01",
+    timestamp: '2022-10-07 11:36:01'
   };
   return data;
 }
@@ -41,7 +41,7 @@ function fetchData() {
 // Removes filler dummy data values
 function removeFiller() {
   //need filler to stop layout shift
-  const fillerTableItems = document.querySelectorAll(".dummyTable");
+  const fillerTableItems = document.querySelectorAll('.dummyTable');
   fillerTableItems.forEach((element) => {
     element.remove();
   });
@@ -50,7 +50,7 @@ function removeFiller() {
 // this function rounds the PV values
 // will also parse values using scientific notation (10.923e-09 -> 10.9e-09)
 function roundNum(num) {
-  let eIndex = num.indexOf("e");
+  let eIndex = num.indexOf('e');
   if (eIndex == -1) {
     return Number(num).toPrecision(4);
   } else {
@@ -62,13 +62,13 @@ function roundNum(num) {
 
 // updates the font size of our twitter widget
 async function updateTwitterFontSize() {
-  const iframe = document.querySelector("#twitter-widget-0");
+  const iframe = document.querySelector('#twitter-widget-0');
   const elementList = iframe.contentWindow.document.querySelectorAll(
-    ".timeline-Tweet-text"
+    '.timeline-Tweet-text'
   );
   for (const node of elementList) {
-    node.style.fontSize = "225%";
-    node.style.lineHeight = "28px";
+    node.style.fontSize = '225%';
+    node.style.lineHeight = '28px';
   }
 }
 
@@ -83,68 +83,68 @@ async function mapData(data) {
 
   // Reads units from dummy dictionary
   const listUnitsDict = Object.entries(data.readUnitsDict);
-  const tableBody = document.querySelector("tbody");
+  const tableBody = document.querySelector('tbody');
 
   //
 
   for (const [index, value] of listPVDict.entries()) {
-    const row = document.createElement("tr");
+    const row = document.createElement('tr');
 
     // assign class based on PV index
 
     // Simple ion source label
     if (index == 2 || index == 1 || index == 0) {
-      row.className = "original sis";
+      row.className = 'original sis';
       if (index == 0) {
-        const beamName = document.createElement("td");
-        beamName.rowSpan = "3";
-        beamName.textContent = "SIS/MWIS";
-        beamName.className = "beamName";
+        const beamName = document.createElement('td');
+        beamName.rowSpan = '3';
+        beamName.textContent = 'SIS/MWIS';
+        beamName.className = 'beamName';
         row.appendChild(beamName);
       }
     }
     // Microwave Ion source labesl
     else if (index == 5 || index == 4 || index == 3) {
-      row.className = "original mcis";
+      row.className = 'original mcis';
       if (index == 3) {
-        const beamName = document.createElement("td");
-        beamName.rowSpan = "3";
-        beamName.textContent = "MCIS";
-        beamName.className = "beamName";
+        const beamName = document.createElement('td');
+        beamName.rowSpan = '3';
+        beamName.textContent = 'MCIS';
+        beamName.className = 'beamName';
         row.appendChild(beamName);
       }
     }
 
     // A/Q variable label
     else if (index == 8 || index == 7 || index == 6) {
-      row.className = "original aq";
+      row.className = 'original aq';
       if (index == 6) {
-        const beamName = document.createElement("td");
-        beamName.rowSpan = "3";
-        beamName.textContent = "A/Q";
-        beamName.className = "beamName";
+        const beamName = document.createElement('td');
+        beamName.rowSpan = '3';
+        beamName.textContent = 'A/Q';
+        beamName.className = 'beamName';
         row.appendChild(beamName);
       }
     }
 
     // Vacuum variable group label
     else if (index == 9 || index == 10 || index == 11) {
-      row.className = "original vacuum";
+      row.className = 'original vacuum';
       if (index == 9) {
-        const beamName = document.createElement("td");
-        beamName.rowSpan = "3";
-        beamName.textContent = "Vacuum";
-        beamName.className = "beamName";
+        const beamName = document.createElement('td');
+        beamName.rowSpan = '3';
+        beamName.textContent = 'Vacuum';
+        beamName.className = 'beamName';
         row.appendChild(beamName);
       }
     } else {
-      row.className = "original";
+      row.className = 'original';
     }
 
     // create table cells
 
-    const nameRow = document.createElement("td");
-    const valueRow = document.createElement("td");
+    const nameRow = document.createElement('td');
+    const valueRow = document.createElement('td');
 
     // PV Name
     nameRow.textContent += value[0];
@@ -163,7 +163,7 @@ async function mapData(data) {
     // }
 
     // table cell for the units
-    const tableCell = document.createElement("td");
+    const tableCell = document.createElement('td');
 
     // map the units
     tableCell.textContent += listUnitsDict[index][1];
@@ -175,46 +175,46 @@ async function mapData(data) {
 
 async function updateGraphData() {
   const graphData = await getGraphData();
-  const hiddenData = document.querySelector("#graph_data");
-  hiddenData.innerHTML = graphData["IOS:FC6:SCALECUR"];
+  const hiddenData = document.querySelector('#graph_data');
+  hiddenData.innerHTML = graphData['IOS:FC6:SCALECUR'];
 }
 
 async function updateSecondColumnTableValues() {
   const data = await getLocalOlisData();
   const listPVDict = Object.entries(data);
-  const tableBody = document.querySelector("tbody");
-  const tableRows = tableBody.querySelectorAll("tr");
+  const tableBody = document.querySelector('tbody');
+  const tableRows = tableBody.querySelectorAll('tr');
   tableRows.forEach((row, index) => {
-    const tableCell = row.querySelector("td:nth-last-child(2)");
+    const tableCell = row.querySelector('td:nth-last-child(2)');
     tableCell.textContent = roundNum(listPVDict[index][1]);
   });
 }
 
 function insertTime() {
   const d = new Date();
-  const element = document.querySelector(".time");
+  const element = document.querySelector('.time');
   element.textContent = `${d.toTimeString()}`;
 }
 
 async function getLaserDirectionData() {
-  const response = await fetch("http://127.0.0.1:8081/direction", {
-    method: "GET",
+  const response = await fetch('http://127.0.0.1:8081/direction', {
+    method: 'GET'
   });
   const freshData = await response.json();
   return freshData;
 }
 
 async function getLocalOlisData() {
-  const response = await fetch("http://127.0.0.1:8081/OLIS", {
-    method: "GET",
+  const response = await fetch('http://127.0.0.1:8081/OLIS', {
+    method: 'GET'
   });
   const freshData = await response.json();
   return freshData;
 }
 
 async function getGraphData() {
-  const response = await fetch("http://127.0.0.1:8081/ChartData", {
-    method: "GET",
+  const response = await fetch('http://127.0.0.1:8081/ChartData', {
+    method: 'GET'
   });
   const freshData = await response.json();
   return freshData;
@@ -222,45 +222,93 @@ async function getGraphData() {
 
 async function setDiagramLabels() {
   const freshData = await getLaserDirectionData();
-  const aData = freshData["IOS:XCB1AW:RDVOL"];
-  const bData = freshData["IOS:XCB1AE:RDVOL"];
-  const cData = freshData["IOS:PSWXCB1A:STATON"];
-  let status = document.querySelector(".diagramLabelBeamStatus");
+  const aData = freshData['IOS:XCB1AW:RDVOL'];
+  const bData = freshData['IOS:XCB1AE:RDVOL'];
+  const cData = freshData['IOS:PSWXCB1A:STATON'];
+  let status = document.querySelector('.diagramLabelBeamStatus');
 
   // Logic for bean source, Don't ask
   if (aData > 400 && bData > 400 && cData == 1) {
-    document.querySelector(".diagram-img").src = "./public/graphic_left.svg";
-    document.querySelector(".diagramLabelBeamSource").innerHTML =
-      "Beam Source: Surface Ion";
-    document.querySelector(".diagramLabelBeamStatus").innerHTML =
-      "Beam Status: ON";
+    document.querySelector('.diagram-img').src = './public/graphic_left.svg';
+    document.querySelector('.diagramLabelBeamSource').innerHTML =
+      'Beam Source: Surface Ion';
+    document.querySelector('.diagramLabelBeamStatus').innerHTML =
+      'Beam Status: ON';
     return;
   }
   if (aData > 400 && bData > 400 && cData == 0) {
-    document.querySelector(".diagram-img").src = "./public/graphic_right.svg";
-    document.querySelector(".diagramLabelBeamSource").innerHTML =
-      "Beam Source: Microwave";
-    document.querySelector(".diagramLabelBeamStatus").innerHTML =
-      "Beam Status: ON";
-    document.querySelector(".diagramLabelBeamStatus");
+    document.querySelector('.diagram-img').src = './public/graphic_right.svg';
+    document.querySelector('.diagramLabelBeamSource').innerHTML =
+      'Beam Source: Microwave';
+    document.querySelector('.diagramLabelBeamStatus').innerHTML =
+      'Beam Status: ON';
+    document.querySelector('.diagramLabelBeamStatus');
     return;
   }
   if (aData <= 400 && bData <= 400) {
-    document.querySelector(".diagram-img").src = "./public/graphic_mid.svg";
-    document.querySelector(".diagramLabelBeamSource").innerHTML =
-      "Beam Source: Multi-Charge Ion";
-    document.querySelector(".diagramLabelBeamStatus").innerHTML =
-      "Beam Status: ON";
+    document.querySelector('.diagram-img').src = './public/graphic_mid.svg';
+    document.querySelector('.diagramLabelBeamSource').innerHTML =
+      'Beam Source: Multi-Charge Ion';
+    document.querySelector('.diagramLabelBeamStatus').innerHTML =
+      'Beam Status: ON';
     return;
   }
-  document.querySelector(".diagram-img").src = "./public/graphic_none.svg";
-  document.querySelector(".diagramLabelBeamSource").innerHTML =
-    "Beam Source: None";
-  document.querySelector(".diagramLabelBeamStatus").innerHTML =
-    "Beam Status: OFF";
+  document.querySelector('.diagram-img').src = './public/graphic_none.svg';
+  document.querySelector('.diagramLabelBeamSource').innerHTML =
+    'Beam Source: None';
+  document.querySelector('.diagramLabelBeamStatus').innerHTML =
+    'Beam Status: OFF';
   return;
 }
 
+let activeSlide = 4;
+
+async function changeActiveSlidePicture() {
+  const slide1 = document.querySelector('.slide1');
+  const slide2 = document.querySelector('.slide2');
+  const slide3 = document.querySelector('.slide3');
+  const slide4 = document.querySelectorAll('.slide4');
+  if (activeSlide == 1) {
+    setTimeout(() => {}, 45000);
+    activeSlide++;
+    slide1.style.display = 'none';
+    slide2.style.display = 'inline-block';
+    slide2.style.width = 'auto';
+    slide2.style.height = '100%';
+    return;
+  }
+  if (activeSlide == 2) {
+    activeSlide++;
+    slide2.style.display = 'none';
+    slide3.style.display = 'inline-block';
+    slide3.style.width = 'auto';
+    slide3.style.height = '100%';
+    return;
+  }
+  if (activeSlide == 3) {
+    activeSlide++;
+    slide3.style.display = 'none';
+    for (const item of slide4) {
+      item.style.display = 'inline-block';
+    }
+    return;
+  }
+  if (activeSlide == 4) {
+    activeSlide = 1;
+    for (const item of slide4) {
+      item.style.display = 'none';
+    }
+
+    slide1.style.display = 'inline-block';
+    slide1.style.width = 'auto';
+    slide1.style.height = '100%';
+    return;
+  }
+}
+
+setInterval(() => {
+  changeActiveSlidePicture();
+}, 15000);
 // Nav bar Clock
 insertTime();
 //clock goes tick tock
