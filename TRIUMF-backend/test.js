aecef47bbfc65f193c3f6df0e4fad86f825f2479
Desmond Ho <kakacake666@gmail.com>
2022-11-04 22:08:21 -0700

    Updated PV items, updated units

--- TRIUMF-backend/test.js
+++ TRIUMF-backend/test.js
@@ -4,9 +4,14 @@ function parse_xml(xml_object) {
   let data = xml_object["elements"][0]["elements"];
 
   for (let i = 0; i < data.length; i++) {
-    elements[data[i]["attributes"]["pv"]] = data[i]["attributes"]["value"];
+    // Issue with repeat values, hard coded fix
+    if (elements[data[i]["attributes"]["pv"]] != undefined) {
+      elements[data[i]["attributes"]["pv"] + " [MWIS]"] =
+        data[i]["attributes"]["value"];
+    } else {
+      elements[data[i]["attributes"]["pv"]] = data[i]["attributes"]["value"];
+    }
   }
-
   // returns an array with
   return [elements, time];
 }
